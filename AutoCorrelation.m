81e234d13f3ba63e604c61ae4f09d2b84a7b36bd
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-02-19 11:47:17 +0530

    Create AutoCorrelation.m

---
+++ AutoCorrelation.m
@@ -0,0 +1,33 @@
+MatLab program to compute the auto correlation of the given sequence and
+verify its properties.
+  
+  MATLAB Program:-
+    
+clc; clear all; close all;
+x=input ('Enter sequence x(n)=');
+Rxx= conv(x,fliplr(x));
+% y=xcorr(x,x);
+disp('rxx=');
+disp(Rxx);
+figure(1);
+stem(Rxx,'filled'); 
+title('Autocorrelation output');
+xlabel('lag index');  ylabel('amplitude');
+
+%verification of properties
+
+Energy=sum(x.^2)
+center_index=ceil(length(Rxx)/2)
+Rxx_0=Rxx(center_index)
+if Rxx_0==Energy
+disp('Rxx(0) gives energy - proved');
+else
+disp('Rxx(0) gives energy - not proved');
+end
+Rxx_right=Rxx(center_index:1:length(Rxx))
+Rxx_left=Rxx(center_index:-1:1)
+if Rxx_right == Rxx_left
+disp('Rxx is even');
+else
+disp('Rxx is odd');
+end
